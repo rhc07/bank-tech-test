a9a19286dd67c6f4a412f42b3b757e3aeebc7632
rhc07 <rorieclarke@gmail.com>
2021-04-08 12:31:22 +0100

    transaction spec is now testing with a random date

--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -4,7 +4,7 @@ require './lib/transactions'
 
 describe 'Transactions' do
   let(:transaction) { Transactions.new }
-  let(:date) { BankAccount::DATE }
+  let(:date) { "12/10/2021" }
 
   describe '#history' do
     it 'is empty on intialization' do
@@ -13,14 +13,14 @@ describe 'Transactions' do
   end
 
   describe '#deposit' do
-    it 'logs a deposit transaction' do
+    it 'logs a deposit transaction with a random date' do
       transaction.add_deposit(3000, date, 1000)
       expect(transaction.history).to include([date, 3000, nil, 1000])
     end
   end
 
   describe '#withdraw' do
-    it 'logs a withdrawal transaction' do
+    it 'logs a withdrawal transaction with a random date' do
       transaction.add_deposit(3000, date, 1000)
       transaction.add_withdraw(1000, date, 4000)
       expect(transaction.history).to include([date, nil, 1000, 4000])
