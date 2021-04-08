5d6cc23099a65f72761f26c363f1edaa4ae622a7
rhc07 <rorieclarke@gmail.com>
2021-04-08 15:51:40 +0100

    refactored using rubocop

--- spec/feature_tests/feature_spec.rb
+++ spec/feature_tests/feature_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require './lib/bank_account'
 
 describe 'FeatureTests' do
@@ -6,7 +8,7 @@ describe 'FeatureTests' do
 
   context 'before executing any methods' do
     it 'Initializes a new users balance at 0' do
-      expect(account.balance).to eq(0)  
+      expect(account.balance).to eq(0)
     end
     it 'Checks the bank statement is empty' do
       expect(account.print_statement).to eq []
@@ -15,30 +17,29 @@ describe 'FeatureTests' do
 
   context 'Using the account methods' do
     before do
-        account.deposit(1000, date)
+      account.deposit(1000, date)
     end
 
     it 'Allows a user to deposit some funds into transaction history with a random date' do
-      expect(account.transaction.history). to eq([{date: date, credit: 1000, debit: nil, balance: 1000}])
+      expect(account.transaction.history).to eq([{ date: date, credit: 1000, debit: nil, balance: 1000 }])
     end
     it 'Allows a user to deposit some funds into transaction history with a random date' do
       account.withdraw(500, date)
-      expect(account.transaction.history). to eq(
-        [
-              {date: date, credit: 1000, debit: nil, balance: 1000}, 
-              {date: date, credit: nil, debit: 500, balance: 500}
-        ]
+      expect(account.transaction.history).to eq(
+        [{ date: date, credit: 1000, debit: nil, balance: 1000 },
+         { date: date, credit: nil, debit: 500, balance: 500 }]
       )
     end
   end
 
   context 'User wants to print a bank statement in the right format' do
-    let(:printed_statement) {
-        "date || credit || debit || balance\n" \
-        "08/04/2021 || 1000 ||  || 1000\n" \
-        "08/04/2021 ||  || 500 || 500\n" \
-        "08/04/2021 || 500 ||  || 1000\n" \
-        "08/04/2021 ||  || 700 || 300\n" \
+    let(:printed_statement) 
+    {
+      "date || credit || debit || balance\n" \
+      "08/04/2021 || 1000 ||  || 1000\n" \
+      "08/04/2021 ||  || 500 || 500\n" \
+      "08/04/2021 || 500 ||  || 1000\n" \
+      "08/04/2021 ||  || 700 || 300\n" \
     }
 
     it 'Bank statement reflects the users transactions' do
